8da7fffa3a3b64652f18e006e620d2704b9fa33f
tprasad <tprasad@TulasiPC.st.com>
2014-04-02 18:10:18 +0530

    Updated

---
+++ src/com/sterling/WelcomeToJava.java
@@ -0,0 +1,17 @@
+/*
+ * Program to Print string on Console. 
+  In this case "Welcome To JAVA !" is the string i am trying to print on Screen.
+ *
+ */
+
+package com.sterling;
+
+/**
+ *
+ * @author spider
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {        
+        System.out.println("Welcome To JAVA !");
+    }
+}
